a4d6cdbb9267dc5533545e2b9f080a8c9a872a7a
Célio Da Silva <celio01t@gmail.com>
2023-08-26 01:54:47 -0300

    FEAT : primeiro Header e configuração da font

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,10 +1,32 @@
+import FacebookSVG from "../img/Iconfacebook";
+import InstagramSVG from "../img/Iconinstagram";
+import YoutubeSVG from "../img/Iconyoutube";
+import style from "./Header.module.css";
+import Container from "./Container";
+
 export default function Header() {
   return (
-    <header>
-      <section>
-        <div> Parcelametno em ate 12x Sem juros</div>
-         <div></div>
-      </section>
+    <header className={style.header}>
+      <Container>
+        <section className={style.sectionfirst}>
+          <div>
+            <p>Parcelamento em ate 12x Sem juros</p>
+            <p>Desconto de 10% á vista</p>
+            <p>Comptra garantida</p>
+          </div>
+          <div>
+            <a href="/">
+              <InstagramSVG />
+            </a>
+            <a href="/">
+              <FacebookSVG />
+            </a>
+            <a href="/">
+              <YoutubeSVG />
+            </a>
+          </div>
+        </section>
+      </Container>
     </header>
   );
 }
